500bf1817a458ab8ee522f4617ade45cc171b94f
James Richmond <jblackenmusic@gmail.com>
2023-09-11 15:38:41 -0600

    added notes

--- game.py
+++ game.py
@@ -6,15 +6,34 @@ guess = 0
 
 class Game():
     def __init__(self, gameSize):
+        # start new game with rabit at random position
         self.gameSize = gameSize
-        self.guess = guess
         self.rabbit = rabbit.Rabbit(gameSize)
         self.rabbit.setPosition(startingPosition)
         print('rabbit started at position ', startingPosition)
 
+        # init game variables
         self.rabbitFound = False
+        self.guess = guess
 
+    # guess which position rabbit is at
+    def makeGuess(self):
 
+        # update guess
+        self.guess += 1   
+        if self.guess > self.gameSize:
+            self.guess = 0
+        
+        print('guessing position: ', self.guess)
+
+        # check guess
+        if self.guess == self.rabbit.position:
+            print('found rabbit at position ', self.rabbit.position)
+
+            #stop loop if rabbit found
+            self.rabbitFound = True            
+
+    # update gets called every frame from main
     def update(self):
 
         if not self.rabbitFound:
@@ -23,14 +42,3 @@ class Game():
 
         self.rabbit.move()
 
-    def makeGuess(self):
-        self.guess += 1   
-
-        if self.guess > self.gameSize:
-            self.guess = 0
-        
-        print('guessing position: ', self.guess)
-
-        if self.guess == self.rabbit.position:
-            print('found rabbit at position ', self.rabbit.position)
-            self.rabbitFound = True
